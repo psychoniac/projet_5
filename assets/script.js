57c79c7d53d1a9040f14e2c17b0dea4a9947057b
psychoniac <jlnko@hotmail.fr>
2023-02-03 18:38:53 +0100

    mis a jour 03/02, essaie de la fonction foreach

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,47 @@
 //creation des variables
 const leftArrow = document.querySelector(".left_arrow");
 const rightArrow = document.querySelector(".right_arrow");
-const bulletsPointContener = document.querySelector(".dots");
 const sliderContener = document.querySelector("#banner");
+const bulletsPointContener = document.querySelector(".dots");
 const textSlide = document.querySelector("p");
-
+const slideNumber = 1;
 const slides = [
   {
+    id: 0,
     image: "slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
+    id: 1,
     image: "slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
+    id: 2,
     image: "slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
+    id: 3,
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
 
+slides.forEach((slide, index) => {
+  console.log(slide.id);
+  
+});
+
 //mis en place d'un eventListener sur les fleches
 leftArrow.addEventListener("click", () => {
-  console.log("left");
+  let slideActive = document.querySelector(".banner-img");
+
+  //chemin du slide actif
+  slideActive.src = "assets/images/slideshow/" + slides[1].image;
 });
+
 rightArrow.addEventListener("click", () => {
   console.log("right");
 });
@@ -40,20 +53,20 @@ const numberSlide = slides.length;
 
 for (let slide = 0; slide < numberSlide; slide++) {
   const bulletPoint = document.createElement("div");
+  //on rattache notre element a la div #dots
   bulletsPointContener.appendChild(bulletPoint);
+  //on injecte la class dot a chaque div crée
   bulletPoint.classList.add("dot");
 }
 //création du bullet point actif
 const activeBulletPoint = document.querySelector(".dot");
 activeBulletPoint.classList.add("dot_selected");
 
-//création des balises images qui vont contenir les slides
-
 for (let slide = 0; slide < numberSlide; slide++) {
-  const imageSlider = document.createElement("img");
+  //création des variables
+  let imageSlider = document.createElement("img");
+  //on rattache la balise img contenant les differents slide a son parent la div #banner
   sliderContener.prepend(imageSlider);
-  //generation du texte du slide
-  textSlide.innerHTML = slides[slide].tagLine;
   //rajout du chemin de l'image
   imageSlider.src = "assets/images/slideshow/" + slides[slide].image;
   //rajout de la classe banner-img
