45ffde0cec0de94b2425f29224e114c3184b3719
psychoniac <jlnko@hotmail.fr>
2023-02-05 10:11:54 +0100

    creation de la fonction changeslide le slider change d'image , maintenant il faut gerer les extremites et les bullets point correspondant aux images

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ const leftArrow = document.querySelector(".left_arrow");
 const rightArrow = document.querySelector(".right_arrow");
 const sliderContener = document.querySelector("#banner");
 const bulletsPointContener = document.querySelector(".dots");
-const textSlide = document.querySelector("p");
+//const textSlide = document.querySelector("p");
 
 const slides = [
   {
@@ -28,54 +28,30 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-/*
-slides.forEach((slide, index) => {
-  console.log(slide.id);
-  
-});*/
 
 
 //comptage des slides
 const numberSlide = slides.length;
 
+
+
 //mis en place d'un eventListener sur les fleches
 leftArrow.addEventListener("click", () => {
- 
   changeSlide(-1);
-});
+
+})
 
 rightArrow.addEventListener("click", () => {
   changeSlide(1);
-  
-});
+})
 
-//let slideActif = document.querySelector(".banner-img").src;
-//le premier slide est le slides avec l'index 0 dans l'array
-//let numeroSlideActif = slideActif.substr(-5); 
-
-
-
+//function qui fait varier les slides 
 let numeroSlide = 0;
 function changeSlide(sens) {
   numeroSlide = numeroSlide + sens;
-  if (numeroSlide < 0) {
-    numeroSlide = numberSlide - 1;
-  }
-  
-  if (numeroSlide > numberSlide - 1) {
-    numeroSlide = 0;
-  }
-  console.log(numeroSlide);
+  let slide = document.querySelector(".banner-img");
+  slide.src = "./assets/images/slideshow/" + (slides[numeroSlide].image);
 }
-
-//il faut d'abord que je pointe sur le numero du slide Actif et que je decrement sur le numero du slide actif
-
-
-
-
-
-
-
 //on boucle sur numberSlide pour avoir le nombre de bullet point correspondant au nombre de slide
 
 for (let slide = 0; slide < numberSlide; slide++) {
@@ -88,15 +64,3 @@ for (let slide = 0; slide < numberSlide; slide++) {
 //création du bullet point actif
 const activeBulletPoint = document.querySelector(".dot");
 activeBulletPoint.classList.add("dot_selected");
-
-
-for (let slide = 0; slide < numberSlide; slide++) {
-  //création des variables
-  let imageSlider = document.createElement("img");
-  //on rattache la balise img contenant les differents slide a son parent la div #banner
-  sliderContener.prepend(imageSlider);
-  //rajout du chemin de l'image
-  imageSlider.src = "assets/images/slideshow/" + slides[0 + numeroSlide].image;
-  //rajout de la classe banner-img
-  imageSlider.classList.add("banner-img");
-}
